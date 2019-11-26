ce610d550378f23923fc05d9e37c85dfec35c5d2
Lycea <Lycea@users.noreply.github.com>
2019-11-26 17:48:44 +0200

    added a small function which preprocesses the tokens and converts single hashes in the line into text

--- scripts/md_parser/parse_md/parser.py
+++ scripts/md_parser/parse_md/parser.py
@@ -1,7 +1,28 @@
 from parse_md.BodyParser import *
 
 class cParser():
+
+    def __preprocess(self,tokens):
+        last_newline = -1
+        consecutive_hash = 0
+        for token_id in range(len(tokens.t_list)):
+            if tokens.t_list[token_id].type == "NEWLINE":
+                last_newline = token_id 
+            elif tokens.t_list[token_id].type == "HASH":
+                if token_id == last_newline+1 or consecutive_hash >0:
+                    consecutive_hash +=1
+                else:
+                    tokens.t_list[token_id].type = "TEXT"
+                    tokens.t_list[token_id].value = "#"
+            else:
+                consecutive_hash = 0
+                    
+
+        return tokens
     def parse(self,tokens):
+        self.__preprocess(tokens)
+        print("preprocess the tokens...")
+
         body =cBodyParser().match(tokens)
         if body == False:
             print("Error while parsing the text")
