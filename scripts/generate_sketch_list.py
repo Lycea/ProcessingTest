9ba68c0c2afc2ea976d43f604dca1b04d02437cb
Lycea <20510874+Lycea@users.noreply.github.com>
2023-07-15 23:14:22 +0200

    Updated sketches / sketch list / sketch scripts

--- scripts/generate_sketch_list.py
+++ scripts/generate_sketch_list.py
@@ -4,20 +4,22 @@ import sys
 
 
 
-def create_page_tree(tree,path_base,depth=0,base=""):
-    print("base:"+path_base)
-    tree["name"]=os.path.basename(path_base)
-    tree["has_info"]=False
+def create_page_tree(tree,path_base, depth=0, base=""):
+    if depth == 0:
+        print(" ")
+    print( (depth*3*" ") + "base:",path_base)
+    tree["name"]     = os.path.basename(path_base)
+    tree["has_info"] = False
 
     found_dirs = False
     for item in sorted(os.listdir(path_base)):
         deeper_path = os.path.join(path_base,item)
-        
-        
+
+        #there are some subpackages there
         if os.path.isdir(deeper_path)==True:
             tree[item]={"has_child":True,"is_sketch":False}
-            print(depth*"  "+"In sub tree..")
-            print(depth*"  "+str(tree))
+            print((depth*3*" ") +"In sub tree..")
+            #print((depth*3*" ") +str(tree))
             create_page_tree(tree[item],deeper_path,depth+1,base)
             print("")
 
@@ -26,19 +28,16 @@ def create_page_tree(tree,path_base,depth=0,base=""):
             if item.find(os.path.basename(path_base))!=-1 and deeper_path.endswith(".html"):
                 tree["is_sketch"]= True
                 tree["path"]=deeper_path.replace(base,"")
-                print(depth*"  "+"Found sketch")
-                print(depth*"  "+str(tree))
+                print("\n", (depth*3*" ") +"Found sketch")
+                for k in tree:
+                    print("  ",(depth*3*" "),k,tree[k])
 
 
     if not found_dirs:
         tree["has_child"]=False
 
-    print(depth*"  "+str(tree)+"\n")
+    #print(depth*"  "+str(tree)+"\n")
     
-         
-
-    
-
 def process_page_tree(tree,txt,depth=0,is_child=False):
     black_list =["is_sketch","name","has_child","has_info","path"]
     for key in sorted(tree.keys()):
@@ -46,34 +45,34 @@ def process_page_tree(tree,txt,depth=0,is_child=False):
             header_level = max(1,min(depth+3,7))
             header_str   = "div"#"h"+str(header_level)
             print(key)
+
             if tree[key]["has_child"] and not tree[key]["is_sketch"]:
                 if is_child:
                     txt+=depth*"  "+"<"+header_str+" class='collaps_title'>"+key+"</"+header_str+">\n<div class='content_div'>"
-                    txt= process_page_tree(tree[key],txt,depth+1,True)
-
+                    txt= "\n"+process_page_tree(tree[key],txt,depth+1,True)
                     txt+=depth*"  "+"</div>\n"
                 else:
                     txt+=depth*"  "+"<"+header_str+" class='collaps_title'>"+key+"</"+header_str+">\n<div class='content_div'>"
-                    txt= process_page_tree(tree[key],txt,depth+1,True)
+                    txt="\n" +process_page_tree(tree[key],txt,depth+1,True)
 
                     txt+=depth*"  "+"</div>\n"
             elif tree[key]["has_child"]and tree[key]["is_sketch"]:
                 pass
             elif not tree[key]["has_child"]and tree[key]["is_sketch"] and tree[key]["path"][-4:]=="html" :
-                
-                #print(tree[key]["path"])
-                
                 if is_child:
-                    txt+=depth*"  "
-                    txt+='<a class="norm" href="'+tree[key]["path"]+'" >'+tree[key]["name"]+'</a></br>'
-                    txt+="\n"
-                else:
-                    txt+=depth*"  "+'<p class="sketch_item">'
+                    #txt+=depth*"  "
+                    #txt+='<a class="norm" href="'+tree[key]["path"]+'" >'+tree[key]["name"]+'</a></br>'
+                    #txt+="\n"
+
+                    txt+=depth*"  "+'<div class="sketch_item">'
                     txt+='<a class="norm" href="'+tree[key]["path"]+'" >'+tree[key]["name"]+'</a>'
-                    txt+="</p>\n"
+                    txt+="</div>\n"
+
+                else:
+                    txt+=depth*"  "+'<div class="sketch_item">'
+                    txt+='<a class="norm" href="'+tree[key]["path"]+'" >'+tree[key]["name"]+'</a>'
+                    txt+="</div>\n"
     
-            
-            
     print(txt)
     return txt
 
@@ -81,23 +80,16 @@ def process_page_tree(tree,txt,depth=0,is_child=False):
 def generate_list(preview=False):
     create_preview_list = preview
 
-
     #find the complete way to the script directory
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    #print(dir_path)
-
-    #find the importent directories
-    base_path = dir_path[:dir_path.rfind(os.sep)]
+    base_path = os.path.split(dir_path)[0]
+    #base_path = dir_path[:dir_path.rfind(os.sep)]
 
     sketch_path = os.path.join(base_path,"sketches")
-    #print(base_path)
-    #print(sketch_path)
 
     #create the sketch overview page
     sketch_list =open(os.path.join(sketch_path,"sketch_list.html"),"w")
 
-
-    
     template_file = open("../templates/html_files/style_default","r")
     base_text = template_file.read()
 
@@ -105,56 +97,33 @@ def generate_list(preview=False):
 
     list_text = "<p>\n"
 
-
-
     list_text = """
-            <style>
-            .collaps_title {
-                          background-color: #777;
-                          color: white;
-                          cursor: pointer;
-                          padding: 18px;
-                          width: 100%;
-                          border: none;
-                          text-align: left;
-                          outline: none;
-                          font-size: 15px;
-                        }
-
-              .active, .collaps_title:hover {
-                          background-color: #555;
-                        }
-
-              .content_div {
-                          padding: 0 18px;
-                          display: none;
-                          overflow: hidden;
-                          background-color: #f1f1f1;
-                        }
-            </style>
             <p>Here you find an overview of all created sketches: </p>"""+list_text
     list_text = '<div class="content">'+list_text
     count = 0
     #find all sketches
-
     sketch_tree={}
     
-
+    print("start checking for sketches ...")
     for item in os.listdir(sketch_path):
-        print("i am here: ",sketch_path,item)
+        print("  checking dir : ",sketch_path,item)
         deeper_path = os.path.join(sketch_path,item)
         
         if os.path.isdir(deeper_path)==True:
-            sketch_tree[item]={"has_child":True,"is_sketch":False}
-            create_page_tree(sketch_tree[item],deeper_path,0,sketch_path+os.path.sep)
-    
+            sketch_tree[item]={
+                "has_child":True,
+                "is_sketch":False
+            }
+            create_page_tree(sketch_tree[item], deeper_path, 0, sketch_path+os.path.sep)
+
+    #TODO Find out why there is a magic 4 ...  it is the amount of sketches found , maybe some old leftovers ?
     if len(sketch_tree.keys())>4:
+            
+        print("\n\ngenerating sketch list")
+        print("  key length ?",len(sketch_tree.keys()))
         list_text=process_page_tree(sketch_tree,list_text)
 
-
-     
-
-
+    
     list_text+="</p>"
     list_text+="</div>"
 
@@ -162,21 +131,32 @@ def generate_list(preview=False):
                   var coll = document.getElementsByClassName("collaps_title");
                   var i;
 
-                  for (i = 0; i < coll.length; i++) {
-                    coll[i].addEventListener("click", function() {
+                  coll.textContent+= "v"
+
+                  for (i = 0; i < coll.length; i++)
+                  {
+                    coll[i].addEventListener("click", function()
+                    {
                       this.classList.toggle("active");
                       var content = this.nextElementSibling;
-                      if (content.style.display === "block") {
+
+                      if(content.style.display === "block")
+                      {
+                        console.log("switching to none")
                         content.style.display = "none";
-                      } else {
+
+                        this.contentText = this.base_text
+                      }
+                      else
+                      {
+                        this.base_test = this.contentText
+                        this.contentText = "v" + this.contentText
                         content.style.display = "block";
                       }
                     });
                   }
                 </script>\n\n\n
                 """
-
-    
 
     #replace placeholders
     base_text = base_text.replace("{PATH_TO_CSS}","../style.css")
@@ -186,7 +166,6 @@ def generate_list(preview=False):
 
     sketch_list.write(base_text)
     sketch_list.close()
-
 
     print("Added list with "+str(count)+" linked sketches")
 
