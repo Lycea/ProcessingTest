d0c7acde31a0c2406a50e765f3356b14c41cb65a
Lycea <20510874+Lycea@users.noreply.github.com>
2023-07-21 00:55:12 +0200

    Added first working base templating system
    - updated config file:
      - removed copying of index.html , since it is now generated
      - added start template variable
    
    - generator:
     - extended generator to parse template files and the TEMPLATE tag
       which recursively extends pages with other templates

--- scripts/page_gen/gen_page.py
+++ scripts/page_gen/gen_page.py
@@ -1,7 +1,7 @@
 import yaml
 import shutil
 import os
-
+import re
 import check_yaml
 
 pjoin = os.path.join
@@ -11,7 +11,9 @@ base_path = "../.."
 config = None
 config_path = os.path.join(base_path, "generator.yaml")
 generatin_base_path = os.path.join( base_path,"generated")
+template_base_path = ""
 
+parsed_templates = {}
 
 def load_yaml():
     with open(config_path) as yaml_handle:
@@ -25,7 +27,63 @@ def check_base_config(config):
     return True
 
 
+def replace_template(argument_list):
+    template_path = pjoin(template_base_path, argument_list[0])
+
+    if template_path in parsed_templates:
+        return parsed_templates[template_path]
+
+    print(template_path)
+    if os.path.exists(template_path):
+        with open(template_path) as template_file:
+            template_text = template_file.read()
+            replaced_text = find_and_process_templates(template_text)
+            parsed_templates[template_path] = replaced_text
+
+            return replaced_text
+
+    else:
+        print(f"ERROR: TEMPLATE NOT FOUND:\n  {template_path}")
+        exit(-1)
+
+
+def find_and_process_templates(text):
+    template_strings = re.findall("{{(.*)}}", text)
+
+    if len(template_strings) > 0:
+        print(f"Found {len(template_strings)} templates")
+
+        for template in template_strings:
+            splitted_template = template.split(":")
+            generation_type = splitted_template[0]
+            generation_args = splitted_template[1:]
+
+            if generation_type == "TEMPLATE":
+                replace_txt = replace_template(generation_args)
+                text=text.replace("{{" +template + "}}", replace_txt)
+    return text
+
+
+def generate_page(template_path):
+    global template_base_path
+    template_base_path = pjoin(base_path, config["config"]["options"].get("template_location","templates"))
+
+    output_path = pjoin(generatin_base_path, template_path+".html")
+
+    with open(pjoin(template_base_path, template_path)) as template_file:
+        base_text = template_file.read()
+        print(base_text)
+
+        output_text = find_and_process_templates(base_text)
+        print("---------------------")
+        print(output_text)
+
+        with open(output_path, "w") as out_file:
+            out_file.write(output_text)
+        
+
 def start_generation():
+    global config
     #first of change the working directory to this folder, to make life easier...
     os.chdir( os.path.dirname(__file__))
 
@@ -57,5 +115,6 @@ def start_generation():
             print(f"Moving files {file} ...")
             shutil.copy( pjoin(base_path, file) , pjoin(generatin_base_path, file))
 
+    generate_page(config["config"]["options"].get("start_template", "default"))
 
 start_generation()
