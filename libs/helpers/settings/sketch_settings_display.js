e611a1d189824b902e100695d4e907d67369b3b1
Lycea <20510874+Lycea@users.noreply.github.com>
2024-05-12 02:40:06 +0200

    Added first variation of "generic" settings importer
    - added a setting loader to include base
      settings based on a parameter
    
    - added base settings (canvas size)
      for full screen and for display (led display ...)
      for usability later on
    
    - added ripples as an example sketch to use the feature ...
      more to come and be adjusted  !

---
+++ libs/helpers/settings/sketch_settings_display.js
@@ -0,0 +1,13 @@
+
+var SketchSettings = SketchSettings || {};
+
+SketchSettings.width  = 256*2
+SketchSettings.height = 400
+SketchSettings.h_scale = 0.5
+
+SketchSettings.prepare = function()
+{
+    scale(SketchSettings.h_scale,1)
+}
+
+console.log("imported display settings !")
---
+++ sketches/ripples/ripples/ripples.js
@@ -0,0 +1,15 @@
+
+function setup() 
+{
+
+	createCanvas(SketchSettings.width,SketchSettings.height);
+	colorMode(HSL,360,100,100);
+}
+
+
+function draw()
+{
+	SketchSettings.prepate()
+    background(0);   
+
+}
